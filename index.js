062fa627669fc167b9a83b18d1f9320f63213ada
areessiapno <61128934+areessiapno@users.noreply.github.com>
2020-02-17 13:18:09 +0800

    added callback and promise function

--- index.js
+++ index.js
@@ -6,5 +6,24 @@ module.exports = {
 			collection[i] = iterate(collection[i]);
 		}
 		return collection;
+	},
+	mapCallback: function (collection, iteration, callback){
+		for (let i=0; i<collection.length; i++){
+			collection[i] = iteration(collection[i]);
+		}
+		callback(collection);
+	},
+	mapPromise: function (collection, iteration) {
+		return new Promise(function(resolve, reject){
+			try{
+				for (let i=0; i<collection.length; i++){
+					collection[i] = iteration(collection[i]);
+				}
+				resolve(collection);
+			}
+			catch(error){
+				reject(error);
+			}
+		})
 	}
 }
--- test.js
+++ test.js
@@ -7,6 +7,27 @@ let square = function(num){
 	return num * num;
 }
 
-let result = _.map([4,8], square);
+let result = _.map([4,6], square)
 
 console.log(result);
+
+
+let resultCallback = _.mapCallback([4,8], square, function(newcollection){
+	console.log(newcollection);
+});
+
+console.log(resultCallback); // undefined
+
+
+let resultPromise = _.mapPromise([4,8], square);
+
+resultPromise
+.then(function (newcollection){
+	console.log(newcollection);
+})
+.catch(function (error){
+	console.log(error);
+});
+
+console.log(resultPromise); // Promise
+
